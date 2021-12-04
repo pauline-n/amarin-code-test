62c98b98a485f951e115159e13ad03e7679f6374
Pauline Nalwanga <paulinon141@gmail.com>
2021-12-04 19:00:22 +0300

    Added more modules

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -18,22 +18,22 @@ function Customer() {
             <tr>
               <td>1</td>
               <td>Mark</td>
-              <td>Otto</td>
+              <td>Tito</td>
               <td>Room 1</td>
               <td>20,000</td>
             </tr>
             <tr>
               <td>2</td>
               <td>Jacob</td>
-              <td>Thornton</td>
-              <td>Room 1</td>
+              <td>Mugisha</td>
+              <td>Room 3</td>
               <td>120,000</td>
             </tr>
             <tr>
               <td>3</td>
               <td>Marie</td>
               <td>Tracy</td>
-              <td>Room 1</td>
+              <td>Room 2</td>
               <td>100,000</td>
             </tr>
           </tbody>
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import Table from "react-bootstrap/Table";
 
 function Pricing() {
     return (
-        <div>
-            <h1>Hello am on pricing</h1>
-        </div>
-    )
+      <div>
+        <h1>Pricing of all our rooms</h1>
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Room Title</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Room 1</td>
+              <td>20,000</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Room 3</td>
+              <td>120,000</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Room 2</td>
+              <td>100,000</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    );
 }
 
 export default Pricing
